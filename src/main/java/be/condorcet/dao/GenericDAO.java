2d4c6eaa9b56421690a2cd0274d714ef1be9ea7a
SafouaneHaddadi <safouane.haddadi@condorcet.be>
2025-12-21 23:00:17 +0100

    feat(vehicle): add PUT /vehicles/{id} endpoint

--- src/main/java/be/condorcet/dao/GenericDAO.java
+++ src/main/java/be/condorcet/dao/GenericDAO.java
@@ -5,9 +5,10 @@ import java.util.List;
 // Interface générique pour les opérations CRUD de base sur n'importe quelle entité JPA
 
 public interface GenericDAO<T, ID> { //T = type de l'entité. ID = type de la clé primaire
-    void create(T entity);
+    T create(T entity);
     T findById(ID id);
-    void update(T entity);
+    //void update(T entity);
+    T update(T entity);
     void delete(ID id);
     List<T> findAll();
 }
