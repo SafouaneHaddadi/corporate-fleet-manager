0ed68226ac3eaaa1c0f2ae52cecf8173ab895e74
SafouaneHaddadi <safouane.haddadi@condorcet.be>
2025-12-23 22:35:21 +0100

    feat: implement login authentication in UserServlet with session management

--- src/main/java/be/condorcet/web/servlet/UserServlet.java
+++ src/main/java/be/condorcet/web/servlet/UserServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import be.condorcet.model.User;
+import jakarta.servlet.http.HttpSession;
 
 
 import java.io.IOException;
@@ -33,9 +34,18 @@ public class UserServlet extends HttpServlet {
                 showRegisterForm(request, response);
                 break;
 
+            case "login":
+                showLoginForm(request, response);
+                break;
+
             default:
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
+    }
+
+    private void showLoginForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/jsp/user/login.jsp")
+                .forward(request, response);
     }
 
     private void showRegisterForm(HttpServletRequest request, HttpServletResponse response)
@@ -56,10 +66,39 @@ public class UserServlet extends HttpServlet {
                 handleRegister(request, response);
                 break;
 
+            case "login":
+                handleLogin(request, response);
+                break;
+
             default:
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
+
+    private void handleLogin(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+           User user = userService.authenticate(username, password);
+
+            HttpSession session = request.getSession();
+
+            session.setAttribute("loggedUser", user); //on stocke l'user dans la session
+
+            response.sendRedirect(request.getContextPath() + "/vehicles?action=list");
+
+        } catch (Exception e) {
+
+            request.setAttribute("errorMessage", e.getMessage());
+
+            request.getRequestDispatcher("/WEB-INF/jsp/user/login.jsp")
+                    .forward(request, response);
+        }
+    }
+
 
     private void handleRegister(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -72,8 +111,7 @@ public class UserServlet extends HttpServlet {
         try {
             userService.registerUser(user);
 
-            response.sendRedirect(request.getContextPath() + "/vehicles?action=list");
-//            response.sendRedirect(request.getContextPath() + "/users?action=login");
+          response.sendRedirect(request.getContextPath() + "/users?action=login");
 
         } catch (BusinessException e) {
 
